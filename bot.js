0e64e9fb814d55cab02fb3bb5cbd06a7ef5d1db1
jamalaldani <45127780+jamalaldani@users.noreply.github.com>
2018-11-17 22:13:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`$play | ma adre`,"http://twitch.tv/Death Shop")
+client.user.setGame(`$play | pro wolf music`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
